25e00cea54c9de5c64f1a3d6465815e6bdf7ab95
Bárbara <barbarafcaputo@gmail.com>
2022-11-26 02:59:36 -0300

    Projeto Final

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// Tentar estabelecer conexão com banco de dados
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://barbara:Babi123456@cluster0.h7pshti.mongodb.net/t2-backend?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    // Conexão estabelecida com sucesso
+    console.log("Conexão estabelecida com sucesso");
+  })
+  .catch((error) => {
+    console.log("Não foi possível conectar com o banco de dados. Abortando.");
+    exit(1);
+  });
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-app.use(logger('dev'));
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const { exit } = require("process");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,179 +1,382 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-//############################### SETORES ##################################
-const setores = [
-  {
-    id: 1,
-    nome: "vestidos",
-  },
-  {
-    id: 2,
-    nome: "macacões",
-  },
-  {
-    id: 3,
-    nome: "croppeds",
-  },
-  {
-    id: 4,
-    nome: "saias",
-  },
-  {
-    id: 5,
-    nome: "blusas",
-  },
-];
-
-router.get("/setores", function (req, res, next) {
-  res.json({ dados: setores });
+/* IMPLEMENTAÇÃO DE LOGIN E GERAÇÃO DE TOKEN (JWT) PARA AUTENTICAÇÃO */
+router.post("/login", async function (req, res, next) {
+  if (req.body.username === "admin" && req.body.password === "senhasecreta") {
+    const jwtToken = await jwt.sign({ usuario: "admin" }, "Babi123456");
+    res.json({ msg: "Usuário logado com sucesso.", token: jwtToken });
+  } else {
+    res.json({ msg: "Credenciais inválidas." });
+  }
 });
 
-router.get("/setores/:id", function (req, res, next) {
-  const setor = setores.filter((setor) => setor.id == req.params.id);
-  if (setor == null || setor == "")
-    return res.json({ erro: "O setor informado não existe" });
-  res.json({ dados: setor });
+//############################### SETORES ##################################
+// const setores = [
+//   {
+//     id: 1,
+//     nome: "vestidos",
+//   },
+//   {
+//     id: 2,
+//     nome: "macacões",
+//   },
+//   {
+//     id: 3,
+//     nome: "croppeds",
+//   },
+//   {
+//     id: 4,
+//     nome: "saias",
+//   },
+//   {
+//     id: 5,
+//     nome: "blusas",
+//   },
+// ];
+
+const setorSchema = new mongoose.Schema({ nome: "string" });
+const setores = mongoose.model("setores", setorSchema);
+
+router.get("/setores", async function (req, res, next) {
+  const data = await setores.find();
+  res.json({ dados: data });
+});
+
+router.get("/setores/:id", async function (req, res, next) {
+  let data;
+  try {
+    data = await setores.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (data === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  res.json({ dados: data });
 });
 
 router.post("/setores", function (req, res, next) {
-  setores.push(req.body.setor);
-  res.json({ msg: "Novo setor salvo com sucesso!" });
-});
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-router.put("/setores/:id", function (req, res, next) {
-  setores.map((setor) => {
-    if (setor.id == req.params.id) {
-      setor = req.body.setor;
-    }
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  const novoDoc = new setores(req.body.setor);
+  novoDoc.save().then(() => {
+    res.json({ msg: "Novo setor salvo com sucesso!" });
   });
-  res.json({ msg: "Setor atualizado com sucesso!" });
 });
 
-router.delete("/setores/:id", function (req, res, next) {
-  setores = setores.filter((setor) => setor.id != req.params.id);
-  res.json({ msg: "Setor excluído com sucesso!" });
+router.put("/setores/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+  try {
+    doc = await setores.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.update(req.body.setor).then(() => {
+    res.json({ msg: "Setor atualizado com sucesso!" });
+  });
+});
+
+router.delete("/setores/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+  try {
+    doc = await setores.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.deleteOne().then(() => {
+    res.json({ msg: "Setor excluído com sucesso!" });
+  });
 });
 
 //############################### CLIENTES ##################################
 
-const clientes = [
-  {
-    id: 1,
-    nome: "Romário",
-    cpf: "000.000.000-00",
-    endereço: "Caminho da felicidade",
-  },
-  {
-    id: 2,
-    nome: "Neymar",
-    cpf: "123.456.789-10",
-    endereço: "Vai jogar bola",
-  },
-  {
-    id: 3,
-    nome: "Papai Noel",
-    cpf: "999.999.999-99",
-    endereço: "Polo Norte",
-  },
-];
+// const clientes = [
+//   {
+//     id: 1,
+//     nome: "Romário",
+//     cpf: "000.000.000-00",
+//     endereço: "Caminho da felicidade",
+//   },
+//   {
+//     id: 2,
+//     nome: "Neymar",
+//     cpf: "123.456.789-10",
+//     endereço: "Vai jogar bola",
+//   },
+//   {
+//     id: 3,
+//     nome: "Papai Noel",
+//     cpf: "999.999.999-99",
+//     endereço: "Polo Norte",
+//   },
+// ];
 
-router.get("/clientes", function (req, res, next) {
-  res.json({ dados: clientes });
+const clienteSchema = new mongoose.Schema({
+  nome: "string",
+  cpf: "string",
+  endereço: "string",
+});
+const clientes = mongoose.model("clientes", clienteSchema);
+
+router.get("/clientes", async function (req, res, next) {
+  const data = await clientes.find();
+  res.json({ dados: data });
 });
 
-router.get("/clientes/:id", function (req, res, next) {
-  const cliente = clientes.filter((cliente) => cliente.id == req.params.id);
-  if (cliente == null || cliente == "")
-    return res.json({ erro: "O cliente informado não existe" });
-  res.json({ dados: cliente });
+router.get("/clientes/:id", async function (req, res, next) {
+  let data;
+  try {
+    data = await clientes.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (data === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  res.json({ dados: data });
 });
 
 router.post("/clientes", function (req, res, next) {
-  clientes.push(req.body.cliente);
-  res.json({ msg: "Novo cliente salvo com sucesso!" });
-});
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-router.put("/clientes/:id", function (req, res, next) {
-  clientes.map((cliente) => {
-    if (cliente.id == req.params.id) {
-      cliente = req.body.cliente;
-    }
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  const novoDoc = new clientes(req.body.cliente);
+  novoDoc.save().then(() => {
+    res.json({ msg: "Novo cliente salvo com sucesso!" });
   });
-  res.json({ msg: "Cliente atualizado com sucesso!" });
 });
 
-router.delete("/clientes/:id", function (req, res, next) {
-  clientes = clientes.filter((cliente) => cliente.id != req.params.id);
-  res.json({ msg: "Cliente excluído com sucesso!" });
+router.put("/clientes/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+  try {
+    doc = await clientes.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.update(req.body.cliente).then(() => {
+    res.json({ msg: "Cliente atualizado com sucesso!" });
+  });
+});
+
+router.delete("/clientes/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+
+  try {
+    doc = await clientes.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.deleteOne().then(() => {
+    res.json({ msg: "Cliente excluído com sucesso!" });
+  });
 });
 
 //############################### PAGAMENTOS ##################################
 
-const pagamentos = [
-  {
-    id: 1,
-    nome: "Pix",
-    valor: "R$600,00",
-    numerodopix: "123456",
-    formaDePagamento: "À vista",
-  },
-  {
-    id: 2,
-    nome: "Cartão de Crédito",
-    valor: "R$1.800,00",
-    numeroDeParcelas: "2",
-    formaDePagamento: "Parcelado",
-  },
-  {
-    id: 3,
-    nome: "Cartão de Débito",
-    valor: "R$300,00",
-    formaDePagamento: "À vista",
-  },
-  {
-    id: 4,
-    nome: "Boleto",
-    valor: "R$850,00",
-    formaDePagamento: "Pagamento não realizado",
-  },
-];
+// const pagamentos = [
+//   {
+//     id: 1,
+//     nome: "Pix",
+//     valor: "R$600,00",
+//     numerodopix: "123456",
+//     formaDePagamento: "À vista",
+//   },
+//   {
+//     id: 2,
+//     nome: "Cartão de Crédito",
+//     valor: "R$1.800,00",
+//     numeroDeParcelas: "2",
+//     formaDePagamento: "Parcelado",
+//   },
+//   {
+//     id: 3,
+//     nome: "Cartão de Débito",
+//     valor: "R$300,00",
+//     formaDePagamento: "À vista",
+//   },
+//   {
+//     id: 4,
+//     nome: "Boleto",
+//     valor: "R$850,00",
+//     formaDePagamento: "Pagamento não realizado",
+//   },
+// ];
 
-router.get("/pagamentos", function (req, res, next) {
-  res.json({ dados: pagamentos });
+const pagamentoSchema = new mongoose.Schema({
+  nome: "string",
+  valor: "string",
+  numeroDeParcelas: "string",
+  formaDePagamento: "string",
+});
+const pagamentos = mongoose.model("pagamentos", pagamentoSchema);
+
+router.get("/pagamentos", async function (req, res, next) {
+  const data = await pagamentos.find();
+  res.json({ dados: data });
 });
 
-router.get("/pagamentos/:id", function (req, res, next) {
-  const pagamento = pagamentos.filter(
-    (pagamento) => pagamento.id == req.params.id
-  );
-  if (pagamento == null || pagamento == "")
-    return res.json({ erro: "O pagamento informado não existe" });
-  res.json({ dados: pagamento });
+router.get("/pagamentos/:id", async function (req, res, next) {
+  let data;
+  try {
+    data = await pagamentos.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (data === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  res.json({ dados: data });
 });
 
 router.post("/pagamentos", function (req, res, next) {
-  pagamentos.push(req.body.pagamento);
-  res.json({ msg: "Pagamento realizado com sucesso!" });
-});
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-router.put("/pagamentos/:id", function (req, res, next) {
-  pagamentos.map((pagamento) => {
-    if (pagamento.id == req.params.id) {
-      pagamento = req.body.pagamento;
-    }
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  const novoDoc = new pagamentos(req.body.pagamento);
+  novoDoc.save().then(() => {
+    res.json({ msg: "Novo pagamento salvo com sucesso!" });
   });
-  res.json({ msg: "Pagamento atualizado com sucesso!" });
 });
 
-router.delete("/pagamentos/:id", function (req, res, next) {
-  pagamentos = pagamentos.filter((pagamento) => pagamento.id != req.params.id);
-  res.json({ msg: "Pagamento excluído com sucesso!" });
+router.put("/pagamentos/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+  try {
+    doc = await pagamentos.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.update(req.body.pagamento).then(() => {
+    res.json({ msg: "Pagamento atualizado com sucesso!" });
+  });
+});
+
+router.delete("/pagamentos/:id", async function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, "Babi123456");
+  } catch (err) {
+    return res.json({ msg: "Usuário não autenticado. Ação negada." });
+  }
+
+  let doc;
+  try {
+    doc = await pagamentos.findById(req.params.id);
+  } catch (e) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  if (doc === null) {
+    return res.json({ msg: "Id inválido." });
+  }
+
+  doc.deleteOne().then(() => {
+    res.json({ msg: "Pagamento excluído com sucesso!" });
+  });
 });
 
 module.exports = router;
